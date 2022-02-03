0f914177d6eea6cdfb6fbac136547dea218b3fd0
Kewejuankenobie <97452684+Kewejuankenobie@users.noreply.github.com>
2022-02-03 12:56:52 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+#Class describing the puzzle
+class puzzle:
+    seed = 0
+    def __init__(self, type):
+        self.type = type
+        #Make the cube in its solved state, Maybe use seperate class for tiles
+
+    def scramble(self, type):
+        pass
+
+    def doMove(self, move, type):
+        pass
+
+    def rotateCube(self, dir, type):
+        pass
+
+#Tile class as super with different types as a sub class
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
