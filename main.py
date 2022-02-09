56fb4b7274bb23a2d8e8ced09b8569f4112cd301
Kewejuankenobie <97452684+Kewejuankenobie@users.noreply.github.com>
2022-02-09 12:56:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-#Class describing the puzzle
+# Class describing the puzzle
 class puzzle:
     seed = 0
+
     def __init__(self, type):
         self.type = type
         self.cubeMatrix = []
-        #Makes a cube represented as a 3D matrix with peice types
+        # Makes a cube represented as a 3D matrix with peice types
         for d in range(type):
             depth = []
             for h in range(type):
@@ -39,28 +40,47 @@ class puzzle:
     def rotateCube(self, dir, type):
         pass
 
-#Tile class as super with different types as a sub class
+
+# Tile class as super with different types as a sub class
 class tile:
+    colors = {"side1": "", "side2": "" , "side3" : ""}
+    rotationVector = (0, 0, 0)
     def __init__(self, depth, height, length):
         self.depth = depth
         self.height = height
         self.length = length
 
+
 class edge(tile):
-    pass
+    def __init__(self, depth, height, length):
+        super().__init__(depth, height, length)
+        if self.depth == 0:
+            self.colors["side1"] = "green"
+        elif self.depth == 2:
+            self.colors["side1"] = "blue"
+        else:
+            pass
+        print(self.colors)
+
+
 
 class center(tile):
     pass
 
+
 class corner(tile):
     pass
 
-#Actual Window
+
+# Actual Window
 class game:
     pass
 
+
 def main():
     puzzle1 = puzzle(3)
+    e = edge(0, 0, 1)
+
 
 if __name__ == '__main__':
     main()
